add95db15c6fb391b720da93f55374f4c80a6b57
Nicolas Liautaud <nicolas.liautaud@gmail.com>
2015-10-23 00:29:45 +0200

    diverse fixes & console infos

--- story_files/main.js
+++ story_files/main.js
@@ -9,7 +9,8 @@ $(function() {
 		isDragging = false,
 		haveChanged = false,
 		isExport = false,
-		linkedFrames = null;
+		linkedFrames = null,
+		sortableParts = [];
 
 	// activate management and contenteditable
 	body.removeClass('nomanagement');
@@ -62,122 +63,6 @@ $(function() {
 	});
 
 
-	//*/// sortable
-
-	Sortable.create(document.querySelector('.scenes'), {
-		animation: 100,
-		draggable: '.scene',
-		handle: '.scene-handle',
-		ghostClass: 'dragged',
-		onStart: function (evt) {
-			isDragging = true;
-			$('.scene').addClass('closed-tmp');
-		},
-		onEnd: function (evt) {
-			isDragging = false;
-			$('.scene').removeClass('closed-tmp');
-			// moved shadow, or after shadow
-			var item = $(evt.item);
-			if(item.hasClass('shadow')) revealShadowScene(item);
-			if(item.prev().hasClass('shadow')) revealShadowScene(item.prev());
-		},
-		onSort: function (evt) {
-			setChanged();
-		}
-	});
-	var sortableParts = [];
-	function initFramesSort() {
-		// destroy old ones
-		for (var i = 0; i < sortableParts.length; i++) {
-			sortableParts[i].destroy();
-		}
-		sortableParts.length = 0;
-		// make new ones
-		[].forEach.call(document.querySelectorAll('.frames'), function (el) {
-			sortableParts.push(Sortable.create(el, {
-				group: 'frames',
-				draggable: '.frame',
-				handle: '.frame-img',
-				chosenClass: 'dragged',
-				ghostClass: 'dragged',
-				animation: 0,
-				onStart: function (evt) {
-					isDragging = true;
-					tagSiblingsOf($(evt.item));
-					delOverlay();
-				},
-				onEnd: function (evt) {
-					isDragging = false;
-					manageMoves($(evt.item));
-				},
-				onSort: function (evt) {
-					setChanged();
-				}
-			}));
-		});
-	}
-
-	function tagSiblingsOf(item) {
-		// tag the frames which are in the same shot
-		// that the given item (cf. manageMoves)
-		item.nextUntil(':not(.sameshot)')
-			.addClass('linked after');
-		if(item.hasClass('sameshot')) {
-			item.prevUntil(':not(.sameshot)')
-				.andSelf().prev()
-				.addClass('linked before');
-		}
-	}
-	function manageMoves(item) {
-		// manage global behaviors of :
-		// - frames moved in their shot,
-		// - frames moved in/out a shot,
-		// - all a shot moved at once.
-		// - shadow frame moved
-		// - frame moved after the shadow
-		var linked = $('.linked'),
-			next = item.next(),
-			prev = item.prev(),
-			prevIsMyShot = prev.hasClass('linked'),
-			nextIsMyShot = next.hasClass('linked'),
-			nextHaveSameshot = next.hasClass('sameshot'),
-			haveSameshot = item.hasClass('sameshot');
-
-		// moved shadow, or after shadow
-		if(item.hasClass('shadow')) revealShadowFrame(item);
-		if(prev.hasClass('shadow')) revealShadowFrame(prev);
-
-		// item was in a shot
-		if(linked.length != 0) {
-			// moved at the head of its shot
-			if(nextIsMyShot && !prevIsMyShot) {
-				item.removeClass('sameshot');
-				next.addClass('sameshot');
-				console.log('moved first of its shot');
-			}
-			// was the first : others follow the chosen one
-			else if(!prevIsMyShot && !haveSameshot) {
-				$('.linked.before').detach().insertBefore(item);
-				$('.linked.after').detach().insertAfter(item);
-				console.log('follow me !');
-			}
-			// moved outside of its shot
-			else if(!prevIsMyShot && !nextIsMyShot) {
-				item.removeClass('sameshot');
-				console.log('bye bye');
-			}
-			linked.removeClass('linked before after');
-		}
-		// is inserted in a shot
-		if(nextHaveSameshot) {
-			item.addClass('sameshot');
-			console.log('moved in a shot');
-		}
-	}
-	initFramesSort();
-
-
-
 	//*/// drop images
 
 	story.delegate('.frame', 'dragenter', delOverlay);
@@ -206,16 +91,15 @@ $(function() {
 		for (var i = 0; i < files.length; i++){
 			var load = loadImage(files[i], target);
 			if(!load) continue;
-			frame = target.closest('.frame');
+			frame = newframe = target.closest('.frame');
 			if(frame.hasClass('shadow')) 
-				newframe = revealShadowFrame(target);
+				newframe = revealFrame(target);
 			else if(i < files.length -1 ) {
 				newframe = newFrame();
 				frame.after(newframe);
 			}
 			target = newframe.find('img');
 		}
-		loadImage(event.originalEvent.dataTransfer.files[0], $(this));
 	});
 
 	function loadImage (file, img) {
@@ -229,24 +113,27 @@ $(function() {
 			setTimeout(function () {
 				frame.removeClass('notimage');
 			}, 200);
+			console.log('not an image');
 			return false;
 		}
 		// let's go
+		console.log('load', file.name);
 		var reader = new FileReader();
 		reader.readAsDataURL(file);
 		reader.addEventListener('loadend', function (e, f) {
-			var canvas = document.createElement('canvas');
-			var context = canvas.getContext('2d');
-			
-			var image = new Image();
+			var canvas = document.createElement('canvas'),
+				context = canvas.getContext('2d')
+				image = new Image();
 			image.src = this.result;
 			image.onload = function() {
 				var width = this.width,
 					height = this.height,
 					max = 4000;
 				// set pano mode
-				if(width > 2 * height)
+				if(width > 2 * height) {
 					frame.addClass('ispano');
+					console.log('enable pano mode');
+				}
 				// animate
 				frame.addClass('dropped');
 				setTimeout(function () {
@@ -254,10 +141,12 @@ $(function() {
 				}, 500);
 				// limit size
 				if(width > max) {
+					console.log('resize width', width);
 					width = max;
 					height = width * (this.height / this.width);
 				}
 				if(height > max) {
+					console.log('resize height', height);
 					height = max;
 					width = height * (this.width / this.height);
 				}
@@ -282,29 +171,25 @@ $(function() {
 
 	//*/// add
 
-	shadowScene = story.find('.scene.shadow');
-	if(!shadowScene.length) shadowScene = story.find('.scenes').append(newShadowScene());
-	story.delegate('.scene.shadow .scene-header *[contenteditable]', 'input', function(e){
-		revealShadowScene($(this));
+	$('.scene').each(function () {
+		revealScene($(this));
 	});
+	addShadowScene();
 
-	story.delegate('.add-scene', 'click', function(e){
-		$(scene_tpl).appendTo('.scenes').hide().slideDown(200);
-		initFramesSort();
-		setChanged();
+	story.delegate('.scene.shadow .scene-header *[contenteditable]', 'input', function(e){
+		revealScene($(this));
 	});
 	story.delegate('.add-frame-before', 'click', function(e){
 		$(this).closest('.scene').find('.frames').prepend(newFrame());
 		setChanged();
+		console.log('prepend a frame');
 	});
 	story.delegate('.add-frame', 'click', function(e){
-		var scene = $(this).closest('.scene'),
-			shadow = scene.find('.frame.shadow');
-		if(!shadow.length) shadow = scene.find('.frames').append(newShadowFrame());
-		revealShadowFrame(shadow);
+		var shadow = $(this).closest('.scene').find('.frame.shadow');
+		revealFrame(shadow);
 	});
 	body.delegate('.frame.shadow *[contenteditable]', 'input', function(e){
-		revealShadowFrame($(this));
+		revealFrame($(this));
 	});
 	function newShadowFrame () {
 		return $(frame_tpl);
@@ -318,24 +203,48 @@ $(function() {
 	function newScene () {
 		return unShadow(newShadowScene());
 	}
+	function isShadow (el) {
+		return el.hasClass('shadow');
+	}
 	function unShadow (el) {
 		return el.removeClass('shadow management');
 	}
-	function revealShadowScene (el) {
-		if(!el.hasClass('scene')) el = el.closest('.scene');
-		var newel = $(scene_tpl);
-		el.parent().append(newel);
-		unShadow(el);
-		setChanged();
-		return newel;
+	function addShadowScene () {
+		// add a new shadow scene if needed
+		var shadowscene = story.find('.scene.shadow');
+		if(shadowscene.length === 0) {
+			shadowscene = newShadowScene();
+			story.find('.scenes').append(shadowscene);
+			initFramesSort();
+			console.log('add a new shadow scene');
+		}
+		return shadowscene;
 	}
-	function revealShadowFrame (el) {
-		if(!el.hasClass('frame')) el = el.closest('.frame');
-		var newel = $(frame_tpl);
-		el.parent().append(newel);
-		unShadow(el);
+	function revealScene (scene) {
+		if(!scene.hasClass('scene'))
+			scene = scene.closest('.scene');
+		if(isShadow(scene)) {
+			unShadow(scene);
+			console.log('reveal a scene');
+		}
+		// add a new shadow frame if needed
+		var shadowframe = scene.find('.shadow');
+		if(shadowframe.length === 0) {
+			scene.find('.frames').append(newShadowFrame());
+			console.log('add a new shadow frame');
+		}
 		setChanged();
-		return newel;
+		return addShadowScene();
+	}
+	function revealFrame (frame) {
+		if(!frame.hasClass('frame'))
+			frame = frame.closest('.frame');
+		if(isShadow(frame)) {
+			unShadow(frame);
+			console.log('reveal a frame');
+		}
+		revealScene(frame);
+		return frame.parent().find('.shadow');
 	}
 
 
@@ -388,6 +297,123 @@ $(function() {
 				setChanged();
 			});
 	});
+
+
+
+	//*/// sortable
+
+	Sortable.create(document.querySelector('.scenes'), {
+		animation: 100,
+		draggable: '.scene',
+		handle: '.scene-handle',
+		ghostClass: 'dragged',
+		onStart: function (evt) {
+			isDragging = true;
+			$('.scene').addClass('closed-tmp');
+		},
+		onEnd: function (evt) {
+			isDragging = false;
+			$('.scene').removeClass('closed-tmp');
+		},
+		onSort: function (evt) {
+			setChanged();
+			// moved shadow, or after shadow
+			var item = $(evt.item);
+			if(item.hasClass('shadow')) revealScene(item);
+			if(item.prev().hasClass('shadow')) revealScene(item.prev());
+		}
+	});
+	function initFramesSort() {
+		// destroy old ones
+		for (var i = 0; i < sortableParts.length; i++) {
+			sortableParts[i].destroy();
+		}
+		sortableParts.length = 0;
+		// make new ones
+		$('.frames').each(function (el) {
+			sortableParts.push(Sortable.create($(this).get(0), {
+				group: 'frames',
+				draggable: '.frame',
+				handle: '.frame-img',
+				chosenClass: 'dragged',
+				ghostClass: 'dragged',
+				animation: 0,
+				onStart: function (evt) {
+					isDragging = true;
+					tagSiblingsOf($(evt.item));
+					delOverlay();
+				},
+				onEnd: function (evt) {
+					isDragging = false;
+					manageMoves($(evt.item));
+					revealScene($(evt.from));
+					revealFrame($(evt.item));
+				},
+				onSort: function (evt) {
+					setChanged();
+				}
+			}));
+		});
+	}
+
+	function tagSiblingsOf(item) {
+		// tag the frames which are in the same shot
+		// that the given item (cf. manageMoves)
+		item.nextUntil(':not(.sameshot)')
+			.addClass('linked after');
+		if(item.hasClass('sameshot')) {
+			item.prevUntil(':not(.sameshot)')
+				.andSelf().prev()
+				.addClass('linked before');
+		}
+	}
+	function manageMoves(item) {
+		// manage global behaviors of :
+		// - frames moved in their shot,
+		// - frames moved in/out a shot,
+		// - all a shot moved at once.
+		// - shadow frame moved
+		// - frame moved after the shadow
+		var linked = $('.linked'),
+			next = item.next(),
+			prev = item.prev(),
+			prevIsMyShot = prev.hasClass('linked'),
+			nextIsMyShot = next.hasClass('linked'),
+			nextHaveSameshot = next.hasClass('sameshot'),
+			haveSameshot = item.hasClass('sameshot');
+
+		// moved shadow, or after shadow
+		if(item.hasClass('shadow')) revealFrame(item);
+		if(prev.hasClass('shadow')) revealFrame(prev);
+
+		// item was in a shot
+		if(linked.length != 0) {
+			// moved at the head of its shot
+			if(nextIsMyShot && !prevIsMyShot) {
+				item.removeClass('sameshot');
+				next.addClass('sameshot');
+				console.log('frame moved 1st of its shot');
+			}
+			// was the first : others follow the chosen one
+			else if(!prevIsMyShot && !haveSameshot) {
+				$('.linked.before').detach().insertBefore(item);
+				$('.linked.after').detach().insertAfter(item);
+				console.log('shot moved (linked follows)');
+			}
+			// moved outside of its shot
+			else if(!prevIsMyShot && !nextIsMyShot) {
+				item.removeClass('sameshot');
+				console.log('frame extracted of a shot');
+			}
+			linked.removeClass('linked before after');
+		}
+		// is inserted in a shot
+		if(nextHaveSameshot) {
+			item.addClass('sameshot');
+			console.log('frame inserted in a shot');
+		}
+	}
+	initFramesSort();
 
 
 
